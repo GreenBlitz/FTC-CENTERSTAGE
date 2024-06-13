1df5f97645fbb149c973bd5fb241a80ffe3d9244
YHerm <0549405422.yh@gmail.com>
2024-06-14 02:31:16 +0300

    yoav - cr changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/arm/ArmCommands.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/arm/ArmCommands.java
@@ -1,12 +1,13 @@
 package org.firstinspires.ftc.teamcode.subsystems.arm;
 
+import com.arcrobotics.ftclib.command.Command;
 import com.arcrobotics.ftclib.command.FunctionalCommand;
 
 import org.firstinspires.ftc.teamcode.Robot;
 
 public class ArmCommands {
 
-    public static FunctionalCommand goToState(ArmState armState) {
+    public static Command goToState(ArmState armState) {
         return new FunctionalCommand(
                 () -> Robot.getInstance().ARM.setState(armState),
                 () -> {},
