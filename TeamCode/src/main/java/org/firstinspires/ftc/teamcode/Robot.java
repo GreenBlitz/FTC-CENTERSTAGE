18b4613ea492e79b34b943372d3b3b3e09e9e225
hillelv <hillel.vider@gmail.com>
2024-06-13 19:11:15 +0300

    hillel- unused import

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.hardware.RevIMU;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
