0abb33fe95769bc1f9d94d41c2dc4d217fa73d32
YHerm <0549405422.yh@gmail.com>
2024-06-14 17:35:41 +0300

    yoav - djdj

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DefaultRaz.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DefaultRaz.java
@@ -11,7 +11,7 @@ public abstract class DefaultRaz extends OpMode {
     public final void init() {
         Robot.init(hardwareMap);
         configureBindings();
-        initialize()
+        initialize();
     }
 
     @Override
