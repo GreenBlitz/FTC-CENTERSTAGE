7bfe05b15735faa2bb452666c7527b5b1922d8cd
GilStein1 <gils.server@gmail.com>
2024-06-15 18:20:48 +0300

    Gil - now is protected

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/launcher/LauncherConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/launcher/LauncherConstants.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode.subsystems.launcher;
 
-public class LauncherConstants {
+class LauncherConstants {
     protected static final String SERVO_ID = "launcher_servo";
     protected static final double RELEASED_POSITION = 1;
     protected static final double CLOSED_POSITION = 0;
