b4e160c6d30f2905be70a2307d8e1ba115de3b75
GilStein1 <gils.server@gmail.com>
2024-06-15 17:23:39 +0300

    Gil - changed the open position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/launcher/LauncherConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/launcher/LauncherConstants.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.subsystems.launcher;
 
 public class LauncherConstants {
     protected static final String SERVO_ID = "launcher_servo";
-    protected static final double OPEN_POSITION = 1;
+    protected static final double OPEN_POSITION = 0.95;
     protected static final double CLOSED_POSITION = 0;
     protected static final double TOLERANCE = 0.01;
 }
