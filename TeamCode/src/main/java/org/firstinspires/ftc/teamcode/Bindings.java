3a233929066b47385c2543d166feda7a2c4f17c0
GilStein1 <151079574+GilStein1@users.noreply.github.com>
2024-06-15 18:07:39 +0300

    Update TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bindings.java
    Co-authored-by: greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bindings.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bindings.java
@@ -11,7 +11,7 @@ public class Bindings {
     private static GamepadWrapper mainGamepad;
     private static GamepadWrapper secondGamepad;
 
-    public static void razLauncherTestBindings(Gamepad gamepad1, Gamepad gamepad2) {
+    public static void razLauncherTest(Gamepad gamepad1, Gamepad gamepad2) {
         mainGamepad = new GamepadWrapper(gamepad1);
         mainGamepad.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER).whenPressed(LauncherCommands.launchFromLauncher());
     }
