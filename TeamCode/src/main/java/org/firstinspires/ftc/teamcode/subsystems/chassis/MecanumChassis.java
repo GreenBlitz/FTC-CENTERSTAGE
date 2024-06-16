d60c627352a10b4d64e49e60d6de51ef8d0d11d9
hillelv <hillel.vider@gmail.com>
2024-06-16 07:44:34 +0300

    hillel- working mecanum drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/chassis/MecanumChassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/chassis/MecanumChassis.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.subsystems.chassis;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.drivebase.RobotDrive;
+import com.arcrobotics.ftclib.geometry.Vector2d;
 import com.arcrobotics.ftclib.hardware.RevIMU;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -20,6 +23,7 @@ public class MecanumChassis extends SubsystemBase {
         this.mecanumDrive = new SampleMecanumDrive(hardwareMap);
         this.imu = new RevIMU(hardwareMap);
         imu.init();
+        MecanumDrive
     }
 
     private Motor getChassisMotor(HardwareMap hardwareMap, String id) {
@@ -33,20 +37,31 @@ public class MecanumChassis extends SubsystemBase {
     }
 
     protected void fieldCentricDrive(double strafeSpeed, double forwardSpeed, double turnSpeed, double gyroAngle) {
-        Vector2D input = clampSpeedsByAngle(strafeSpeed, forwardSpeed, gyroAngle);
 
-        strafeSpeed = Range.clip(input.getX(), ChassisConstants.MIN_SPEED_POSSIBLE_VALUE, ChassisConstants.MAX_SPEED_POSSIBLE_VALUE);
-        forwardSpeed = Range.clip(input.getY(), ChassisConstants.MIN_SPEED_POSSIBLE_VALUE, ChassisConstants.MAX_SPEED_POSSIBLE_VALUE);
+        strafeSpeed = Range.clip(strafeSpeed, ChassisConstants.MIN_SPEED_POSSIBLE_VALUE, ChassisConstants.MAX_SPEED_POSSIBLE_VALUE);
+        forwardSpeed = Range.clip(forwardSpeed, ChassisConstants.MIN_SPEED_POSSIBLE_VALUE, ChassisConstants.MAX_SPEED_POSSIBLE_VALUE);
         turnSpeed = Range.clip(turnSpeed, ChassisConstants.MIN_SPEED_POSSIBLE_VALUE, ChassisConstants.MAX_SPEED_POSSIBLE_VALUE);
+
+        Vector2d input = new Vector2d(strafeSpeed, forwardSpeed);
+        input = input.rotateBy(-gyroAngle);
+
+        double theta = input.angle();
 
         double[] wheelSpeeds = new double[4];
 
-        wheelSpeeds[ChassisConstants.FRONT_LEFT_INDEX] = forwardSpeed + strafeSpeed + turnSpeed;
-        wheelSpeeds[ChassisConstants.FRONT_RIGHT_INDEX] = forwardSpeed - strafeSpeed - turnSpeed;
-        wheelSpeeds[ChassisConstants.BACK_LEFT_INDEX] = forwardSpeed - strafeSpeed + turnSpeed;
-        wheelSpeeds[ChassisConstants.BACK_RIGHT_INDEX] = forwardSpeed + strafeSpeed - turnSpeed;
+        wheelSpeeds[ChassisConstants.FRONT_LEFT_INDEX] = Math.sin(theta + Math.PI / 4);;
+        wheelSpeeds[ChassisConstants.FRONT_RIGHT_INDEX] = Math.sin(theta - Math.PI / 4);
+        wheelSpeeds[ChassisConstants.BACK_LEFT_INDEX] = Math.sin(theta - Math.PI / 4);
+        wheelSpeeds[ChassisConstants.BACK_RIGHT_INDEX] = Math.sin(theta + Math.PI / 4);
 
-        clampOutOfRangeSpeeds(wheelSpeeds);
+        normalize(wheelSpeeds, input.magnitude());
+
+        wheelSpeeds[ChassisConstants.FRONT_LEFT_INDEX] += turnSpeed;
+        wheelSpeeds[ChassisConstants.FRONT_RIGHT_INDEX] -= turnSpeed;
+        wheelSpeeds[ChassisConstants.BACK_LEFT_INDEX] += turnSpeed;
+        wheelSpeeds[ChassisConstants.BACK_RIGHT_INDEX] -= turnSpeed;
+
+        normalize(wheelSpeeds);
 
         mecanumDrive.setMotorPowers(
                 wheelSpeeds[ChassisConstants.FRONT_LEFT_INDEX],
@@ -57,22 +72,34 @@ public class MecanumChassis extends SubsystemBase {
 
     }
 
-    private Vector2D clampSpeedsByAngle(double strafeSpeed, double forwardSpeed, double gyroAngle) {
-        return new Vector2D(
-                strafeSpeed * Math.cos(gyroAngle) - forwardSpeed * Math.sin(gyroAngle),
-                strafeSpeed * Math.sin(gyroAngle) + forwardSpeed * Math.cos(gyroAngle)
-        );
+    protected void normalize(double[] wheelSpeeds, double magnitude) {
+        double maxMagnitude = Math.abs(wheelSpeeds[0]);
+        for (int i = 1; i < wheelSpeeds.length; i++) {
+            double temp = Math.abs(wheelSpeeds[i]);
+            if (maxMagnitude < temp) {
+                maxMagnitude = temp;
+            }
+        }
+        for (int i = 0; i < wheelSpeeds.length; i++) {
+            wheelSpeeds[i] = (wheelSpeeds[i] / maxMagnitude) * magnitude;
+        }
+
     }
 
-    private void clampOutOfRangeSpeeds(double[] wheelSpeeds) {
-        double max = ChassisConstants.MAX_SPEED_POSSIBLE_VALUE;
-        for (double wheelSpeed : wheelSpeeds)
-            max = Math.max(max, Math.abs(wheelSpeed));
-
-        if (max > ChassisConstants.MAX_SPEED_POSSIBLE_VALUE) {
-            for (int i = 0; i < wheelSpeeds.length; i++)
-                wheelSpeeds[i] /= max;
+    protected void normalize(double[] wheelSpeeds) {
+        double maxMagnitude = Math.abs(wheelSpeeds[0]);
+        for (int i = 1; i < wheelSpeeds.length; i++) {
+            double temp = Math.abs(wheelSpeeds[i]);
+            if (maxMagnitude < temp) {
+                maxMagnitude = temp;
+            }
         }
+        if (maxMagnitude > 1) {
+            for (int i = 0; i < wheelSpeeds.length; i++) {
+                wheelSpeeds[i] = (wheelSpeeds[i] / maxMagnitude);
+            }
+        }
+
     }
 
     protected void stop() {
