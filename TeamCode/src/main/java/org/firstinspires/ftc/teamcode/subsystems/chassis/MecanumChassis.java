4ed9e685413b75a9de7ae3d7a6f6b32a074c46e4
hillelv <hillel.vider@gmail.com>
2024-06-16 14:50:49 +0300

    hillel- correct

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/chassis/MecanumChassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/chassis/MecanumChassis.java
@@ -15,10 +15,10 @@ public class MecanumChassis extends SubsystemBase {
     private final RevIMU imu;
 
     public MecanumChassis(HardwareMap hardwareMap) {
-        Motor frontLeft = new Motor(hardwareMap, ChassisConstants.FRONT_LEFT_ID);
-        Motor frontRight = new Motor(hardwareMap, ChassisConstants.FRONT_RIGHT_ID);
-        Motor backLeft = new Motor(hardwareMap, ChassisConstants.BACK_LEFT_ID);
-        Motor backRight = new Motor(hardwareMap, ChassisConstants.BACK_RIGHT_ID);
+        Motor frontLeft = getChassisMotor(hardwareMap, ChassisConstants.FRONT_LEFT_ID);
+        Motor frontRight = getChassisMotor(hardwareMap, ChassisConstants.FRONT_RIGHT_ID);
+        Motor backLeft = getChassisMotor(hardwareMap, ChassisConstants.BACK_LEFT_ID);
+        Motor backRight = getChassisMotor(hardwareMap, ChassisConstants.BACK_RIGHT_ID);
 
         this.mecanumDrive = new MecanumDrive(frontLeft, frontRight, backLeft, backRight);
         this.imu = new RevIMU(hardwareMap);
