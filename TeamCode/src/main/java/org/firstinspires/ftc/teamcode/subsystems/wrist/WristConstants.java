395112c6c6aa3838982d47988f10c7f93fc745b8
GreenBlitz <greenblitz4590@gmail.com>
2024-06-16 12:35:00 +0300

    yoav

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/wrist/WristConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/wrist/WristConstants.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.subsystems.wrist;
 class WristConstants {
 
     protected static String SERVO_HARDWARE_NAME = "wrist_gripper";
+
     protected static double SCORE_POSITION = 0;
     protected static double INTAKE_POSITION = 1;
 
