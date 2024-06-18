02bc037ed9c1391dbf0a705057eb149b9684de4c
GreenBlitz <greenblitz4590@gmail.com>
2024-06-18 08:59:08 +0300

    yoav - naming

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/arm/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/arm/Arm.java
@@ -13,12 +13,12 @@ public class Arm extends SubsystemBase {
     private final DcMotorEx motor;
     private final PIDController pidController;
     private ArmState currentState;
-    private ArmState lasState;
+    private ArmState lastState;
 
     public Arm(HardwareMap hardwareMap) {
         this.motor = hardwareMap.get(DcMotorEx.class, ArmConstants.ARM_MOTOR_ID);
         this.currentState = ArmState.STARTING;
-        this.lasState = currentState;
+        this.lastState = currentState;
         this.pidController = ArmConstants.PID_CONTROLLER;
         configMotor();
         configPidController();
@@ -37,7 +37,7 @@ public class Arm extends SubsystemBase {
 
 
     protected void setState(ArmState targetState) {
-        lasState = currentState;
+        lastState = currentState;
         currentState = targetState;
         updateTargetByState();
     }
@@ -66,7 +66,7 @@ public class Arm extends SubsystemBase {
 
     public void telemetry(Telemetry telemetry) {
         telemetry.addData("Arm Current State: ", currentState);
-        telemetry.addData("Arm Last State: ", lasState);
+        telemetry.addData("Arm Last State: ", lastState);
         telemetry.addData("Arm Is At State: ", isAtState());
         telemetry.addData("Arm Current Position: ", motor.getCurrentPosition());
         telemetry.addData("Arm Target Position: ", pidController.getSetPoint());
