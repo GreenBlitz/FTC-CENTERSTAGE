6a868b0e0820a96aee11e0366513b1bda18ac04c
YoniKiriaty <YoniKiriaty@gmail.com>
2024-06-20 11:35:51 +0300

    yoav- chceck

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/functionstesters/RazClimbTest.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.opmodes.functionstesters;
+
+import org.firstinspires.ftc.teamcode.Bindings;
+import org.firstinspires.ftc.teamcode.opmodes.DefaultRaz;
+
+public class RazClimbTest extends DefaultRaz {
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void configureBindings() {
+        Bindings.razClimbTest(gamepad1, gamepad2);
+    }
+
+}
