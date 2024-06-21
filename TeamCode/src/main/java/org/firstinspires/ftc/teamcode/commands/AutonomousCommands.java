fc77c6c2c570b39303b89791e41ac0cf9e473ae3
GreenBlitz <greenblitz4590@gmail.com>
2024-06-21 18:41:25 +0300

    danna - i have no idea what i changed but i dont want to stash

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/AutonomousCommands.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/AutonomousCommands.java
@@ -3,23 +3,19 @@ package org.firstinspires.ftc.teamcode.commands;
 import com.arcrobotics.ftclib.command.Command;
 import com.arcrobotics.ftclib.command.ParallelCommandGroup;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
-import com.arcrobotics.ftclib.command.Subsystem;
 
-import org.checkerframework.checker.units.qual.C;
 import org.firstinspires.ftc.teamcode.subsystems.arm.ArmCommands;
 import org.firstinspires.ftc.teamcode.subsystems.arm.ArmState;
 import org.firstinspires.ftc.teamcode.subsystems.claw.ClawCommands;
 import org.firstinspires.ftc.teamcode.subsystems.wrist.WristCommands;
 import org.firstinspires.ftc.teamcode.subsystems.wrist.WristState;
 
-import java.util.Set;
-
 public class AutonomousCommands {
 
-    public static Command scoreAutonomous(Boolean isAtRightClaw, ArmState armState, WristState wristState){
+    public static Command scoreAutonomous(Boolean isAtRightClaw, ArmState armState, WristState wristState) {
         Command command;
 
-        if (isAtRightClaw){
+        if (isAtRightClaw) {
             command = ClawCommands.toggleRightFinger();
         } else {
             command = ClawCommands.toggleLeftFinger();
