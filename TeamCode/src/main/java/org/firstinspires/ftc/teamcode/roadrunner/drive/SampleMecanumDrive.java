ba50d1638ad884b8aec562db83234dcbe7e6af71
GreenBlitz <greenblitz4590@gmail.com>
2024-06-22 23:22:20 +0300

    hillel - merge master

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/SampleMecanumDrive.java
@@ -57,6 +57,7 @@ import static org.firstinspires.ftc.teamcode.roadrunner.drive.DriveConstants.kV;
  */
 @Config
 public class SampleMecanumDrive extends MecanumDrive {
+
     public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(100, 0, 0);
     public static PIDCoefficients HEADING_PID = new PIDCoefficients(-10, 0, 0);
 
@@ -326,4 +327,5 @@ public class SampleMecanumDrive extends MecanumDrive {
     public static TrajectoryAccelerationConstraint getAccelerationConstraint(double maxAccel) {
         return new ProfileAccelerationConstraint(maxAccel);
     }
+
 }
