b82a632058039461063d5f982090a7ca7246658e
GilStein1 <gils.server@gmail.com>
2024-06-23 15:42:11 +0300

    Gil - changed supplier to be protected

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/RobotStateSupplier.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/RobotStateSupplier.java
@@ -1,10 +1,9 @@
 package org.firstinspires.ftc.teamcode.utils;
 
-import org.firstinspires.ftc.teamcode.Robot;
 import org.firstinspires.ftc.teamcode.RobotState;
 
 public interface RobotStateSupplier {
 
-    public RobotState getRobotState();
+    RobotState getRobotState();
 
 }
