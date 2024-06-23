32a1e513ed62e1e7fb259aa07b7031a0e9071835
GreenBlitz <greenblitz4590@gmail.com>
2024-06-23 17:23:39 +0300

    yoa v- needs test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/Duo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/Duo.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.opmodes.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Bindings;
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.RobotState;
 import org.firstinspires.ftc.teamcode.opmodes.DefaultRaz;
 
 @TeleOp(name = "Duo")
@@ -10,7 +12,7 @@ public class Duo extends DefaultRaz {
 
     @Override
     public void initialize() {
-
+        Robot.getInstance().setState(RobotState.DRIVE).schedule();
     }
 
     @Override
