fba90fa372b783c34476f3e92aff885956fb0a74
GreenBlitz <greenblitz4590@gmail.com>
2024-06-23 12:25:22 +0300

    hillel - comment out

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/DefaultGil.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/DefaultGil.java
@@ -1,23 +1,48 @@
 package org.firstinspires.ftc.teamcode.autos;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.Alliance;
 import org.firstinspires.ftc.teamcode.FieldStartingLocation;
 import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.roadrunner.trajectorysequence.TrajectorySequence;
 
-public abstract class DefaultGil extends LinearOpMode {
+@Autonomous(name = "DefaultGil")
+public  class DefaultGil extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        Robot.getInstance().setAlliance(getAlliance());
-        Robot.getInstance().setFieldStartingLocation(getFieldStartingLocation());
+//        Robot.getInstance().setAlliance(getAlliance());
+//        Robot.getInstance().setFieldStartingLocation(getFieldStartingLocation());
         Robot.init(hardwareMap);
-        run();
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Pose2d startPose = new Pose2d(0, 0, 0);
+
+        drive.setPoseEstimate(startPose);
+
+        TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                .forward(30)
+                .turn(Math.toRadians(90))
+                .forward(30)
+                .turn(Math.toRadians(90))
+                .forward(30)
+                .turn(Math.toRadians(90))
+                .forward(30)
+                .turn(Math.toRadians(90))
+                .build();
+
+        waitForStart();
+
+        if (!isStopRequested())
+            drive.followTrajectorySequence(trajSeq);
     }
 
-    public abstract Alliance getAlliance();
-    public abstract FieldStartingLocation getFieldStartingLocation();
-    public abstract void run() throws InterruptedException;
+//    public abstract Alliance getAlliance();
+//    public abstract FieldStartingLocation getFieldStartingLocation();
+//    public abstract void run() throws InterruptedException;
 
 }
