b21e1819f4f52fc11f5993f13615c1d2f8cd66f5
GreenBlitz <greenblitz4590@gmail.com>
2024-06-23 13:11:07 +0300

    hillel - all positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gamelayout/FieldConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gamelayout/FieldConstants.java
@@ -7,30 +7,30 @@ import org.firstinspires.ftc.teamcode.Robot;
 
 public class FieldConstants {
 
-    public static final Pose2d FAR_RED_LEFT_POSE = null;
-    public static final Pose2d FAR_RED_CENTER_POSE = null;
-    public static final Pose2d FAR_RED_RIGHT_POSE = null;
-    public static final Pose2d FAR_BLUE_LEFT_POSE = null;
-    public static final Pose2d FAR_BLUE_CENTER_POSE = null;
-    public static final Pose2d FAR_BLUE_RIGHT_POSE = null;
-    public static final Pose2d CLOSE_RED_LEFT_POSE = null;
-    public static final Pose2d CLOSE_RED_CENTER_POSE = null;
-    public static final Pose2d CLOSE_RED_RIGHT_POSE = null;
-    public static final Pose2d CLOSE_BLUE_LEFT_POSE = null;
-    public static final Pose2d CLOSE_BLUE_CENTER_POSE = null;
-    public static final Pose2d CLOSE_BLUE_RIGHT_POSE = null;
+    public static final Pose2d FAR_RED_LEFT_POSE = new Pose2d(-40, -40, Math.toRadians(135));
+    public static final Pose2d FAR_RED_CENTER_POSE = new Pose2d(-35, -35, Math.toRadians(90));
+    public static final Pose2d FAR_RED_RIGHT_POSE = new Pose2d(-30, -40, Math.toRadians(45));
+    public static final Pose2d FAR_BLUE_LEFT_POSE = new Pose2d(-40, 40, Math.toRadians(225));
+    public static final Pose2d FAR_BLUE_CENTER_POSE = new Pose2d(-35, 35, Math.toRadians(270));
+    public static final Pose2d FAR_BLUE_RIGHT_POSE = new Pose2d(-30, 40, Math.toRadians(315));
+    public static final Pose2d CLOSE_RED_LEFT_POSE = new Pose2d(5, -40, Math.toRadians(135));
+    public static final Pose2d CLOSE_RED_CENTER_POSE = new Pose2d(10, -35, Math.toRadians(90));
+    public static final Pose2d CLOSE_RED_RIGHT_POSE = new Pose2d(15, -40, Math.toRadians(45));
+    public static final Pose2d CLOSE_BLUE_LEFT_POSE = new Pose2d(15, 40, Math.toRadians(315));
+    public static final Pose2d CLOSE_BLUE_CENTER_POSE = new Pose2d(10, 35, Math.toRadians(270));
+    public static final Pose2d CLOSE_BLUE_RIGHT_POSE = new Pose2d(5, 40, Math.toRadians(225));
 
-    public static final Pose2d RED_LEFT_BACKDROP_POSE = null;
-    public static final Pose2d RED_CENTER_BACKDROP_POSE = null;
-    public static final Pose2d RED_RIGHT_BACKDROP_POSE = null;
-    public static final Pose2d BLUE_LEFT_BACKDROP_POSE = null;
-    public static final Pose2d BLUE_CENTER_BACKDROP_POSE = null;
-    public static final Pose2d BLUE_RIGHT_BACKDROP_POSE = null;
+    public static final Pose2d RED_LEFT_BACKDROP_POSE = new Pose2d(50, -25, Math.toRadians(180));
+    public static final Pose2d RED_CENTER_BACKDROP_POSE = new Pose2d(50, -35, Math.toRadians(180));
+    public static final Pose2d RED_RIGHT_BACKDROP_POSE = new Pose2d(50, -45, Math.toRadians(180));
+    public static final Pose2d BLUE_LEFT_BACKDROP_POSE = new Pose2d(50, 25, Math.toRadians(180));
+    public static final Pose2d BLUE_CENTER_BACKDROP_POSE = new Pose2d(50, 35, Math.toRadians(180));
+    public static final Pose2d BLUE_RIGHT_BACKDROP_POSE = new Pose2d(50, 45, Math.toRadians(180));
 
-    public static final Pose2d FAR_RED_START = null;
-    public static final Pose2d CLOSE_RED_START = null;
-    public static final Pose2d FAR_BLUE_START = null;
-    public static final Pose2d CLOSE_BLUE_START = null;
+    public static final Pose2d FAR_RED_START = new Pose2d(-35, -60, Math.toRadians(90));
+    public static final Pose2d CLOSE_RED_START = new Pose2d(10, -60, Math.toRadians(90));
+    public static final Pose2d FAR_BLUE_START = new Pose2d(-35, 60, Math.toRadians(270));
+    public static final Pose2d CLOSE_BLUE_START = new Pose2d(10, 60, Math.toRadians(270));
 
     private static final Rotation2d BOARD_ANGLE = Rotation2d.fromDegrees(90);
 
